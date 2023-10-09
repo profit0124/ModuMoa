a24a90b4e033f0b621aed5088efe06ac493698d6
profit0124 <profit0124@gmail.com>
2023-10-09 21:51:51 +0900

    Initial Commit

---
+++ ModuMoa/ModuMoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModuMoaApp.swift
+//  ModuMoa
+//
+//  Created by Sooik Kim on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ModuMoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
