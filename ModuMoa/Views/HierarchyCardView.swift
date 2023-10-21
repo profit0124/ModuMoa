15b6448681062f0365e02877d66402b8923a807d
profit0124 <profit0124@gmail.com>
2023-10-21 17:51:34 +0900

    feature: add hierarchyCardView

--- ModuMoa/Views/HierarchyCardView.swift
+++ ModuMoa/Views/HierarchyCardView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct HierarchyCardView: View {
+    
+    @State var parents:[String] = ["", ""]
+    @State var childrens:[String] = ["", ""]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 80) {
+            HStack {
+                ForEach(parents, id: \.self) { _ in
+                    cardViewWithButton()
+                        .frame(width: 250)
+                }
+            }
+            
+            HStack {
+                ForEach(childrens, id: \.self) { _ in
+                    cardViewWithButton()
+                        .frame(width: 250)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func cardViewWithButton() -> some View {
+        VStack {
+            CardView()
+            Button(action: {
+                childrens.append("")
+            }, label: {
+                Image(systemName: "plus")
+                    .foregroundStyle(.white)
+                    .padding(4)
+                    .background{
+                        Circle()
+                            .fill(.disableText)
+                    }
+            })
+        }
     }
 }
-
 #Preview {
     HierarchyCardView()
 }
