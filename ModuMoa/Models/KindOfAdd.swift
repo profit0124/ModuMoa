bdf4a7685f930ae904ca06b3a658a031b0507ca4
profit <profit0124@gmail.com>
2024-07-20 18:51:44 +0900

    feature: 가족 추가 케이스 추가

---
+++ ModuMoa/Models/KindOfAdd.swift
@@ -0,0 +1,15 @@
+//
+//  KindOfAdd.swift
+//  ModuMoa
+//
+//  Created by Sooik Kim on 7/20/24.
+//
+
+import Foundation
+
+enum KindOfAdd: String, CaseIterable {
+    case leftParent = "아버지"
+    case rightParent = "어머니"
+    case son = "아들"
+    case daughter = "딸"
+}
