9928f92f7491c0223fc6486ba8693eaa28f44e5c
profit <profit0124@gmail.com>
2024-08-02 16:32:38 +0900

    refactor: 자식노드 설정 로직 변경

--- ModuMoa/ViewModels/RootViewModel.swift
+++ ModuMoa/ViewModels/RootViewModel.swift
@@ -114,7 +114,7 @@ final class RootViewModel {
     func setChildrenOfNode(_ node: Node) {
         if let partner = node.partner {
             let children = Array(Set(node.children + partner.children).sorted(by: { $0.member.birthday ?? Date() <= $1.member.birthday ?? Date()}))
-            node.children = partner.children
+            node.children = children
             partner.children = children
             if !children.isEmpty {
                 for child in node.children {
